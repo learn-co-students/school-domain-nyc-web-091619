886008babf6bc014974ccc21590ae1ca43b21ece
Altaf Quadri <altafmquadri@gmail.com>
2019-09-17 18:39:08 -0400

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,34 @@
-# code here!
+require 'pry'
+
+class School
+
+    attr_accessor :roster
+
+    def initialize(school_name)
+        @school_name = school_name
+        @roster = {}
+    end
+
+    def add_student(student, grade)
+        @student = student
+        @grade = grade
+
+        if roster[grade] 
+            roster[grade] << student
+        else 
+            roster[grade] = [] 
+            roster[grade].push(student)
+        end
+    end
+    
+    def grade(grade)
+        roster[grade]
+    end
+
+    def sort
+        roster.each {|grade, student| student.sort!}
+    end
+end
+
+
+school = School.new("Bayside High School")
