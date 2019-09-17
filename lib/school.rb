b1dc77e9793d7b50ce4361429f8f611ae4d9aa16
Ashley Karhoff <ashley.karhoff1@gmail.com>
2019-09-17 17:01:20 -0400

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,35 @@
-# code here!
+class School
+    attr_reader :name, :roster
+    attr_writer :name, :roster
+
+    def initialize(name)
+        @name = name
+        @roster = {}
+    end
+
+    def add_student(name, grade)
+        if @roster[grade]
+            @roster[grade] << name
+        else
+            @roster[grade] = [name]
+        end
+    end
+
+    def grade(grade)
+        @roster[grade]
+    end
+
+    def sort
+        # Sorts the keys
+        @roster.sort
+
+        # Iterate keys, sorting all values
+        sorted_roster = @roster.each { |key, value|
+            @roster[key] = value.sort
+        }
+
+        # Return sorted roster
+        sorted_roster 
+    end
+end
+
