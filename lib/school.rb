8e8928b58015708115f82b276e39228699a2ddc5
github username <github email address>
2019-09-18 05:45:56 -0400

    update

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,24 @@
 # code here!
+
+
+class School
+
+    attr_accessor :roster, :student, :grade
+
+    def initialize(name)
+        @name = name
+        @roster = []
+    end
+
+    def add_student(student, grade)
+        if !grade.to_i 
+            @grade = []
+        end
+        grade << student
+    end
+
+
+end
+
+school = School.new("Bayside High School")
+
