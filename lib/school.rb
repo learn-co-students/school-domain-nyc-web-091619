57ddb9ffa85067df59ab6cdb837f158fb9f52a83
Connor Meth <connormeth27@gmail.com>
2019-09-18 08:41:55 -0400

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,28 @@
-# code here!
+#require 'pry'
+
+class School
+
+    attr_reader :name, :grade, :school, :roster
+    
+    def initialize(name)
+        @name = name
+        @roster = Hash.new
+    end
+
+    def add_student(name, grade)
+        @roster[grade] = [] unless @roster[grade]
+        @roster[grade] << name
+    end
+
+    def grade(grade)
+       @roster[grade]
+    end
+
+    def sort
+        @roster.each_value { |v| v.sort! }
+    end
+
+end
+
+
+
